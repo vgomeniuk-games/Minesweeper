a8d7b2164fe3628fff69693e95033413da7b758a
vgomeniuk <vgomeniuk@lvivsoft.com>
2017-07-03 10:01:27 +0300

    Load tiles and draw basic grid

--- main.cpp
+++ main.cpp
@@ -1,9 +1,49 @@
-#include <iostream>
+#include <time.h>
+#include <SFML/Graphics.hpp>
 
-using namespace std;
+int WIDTH = 32;
+int GRID[12][12];
+int SHOWN[12][12];
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    // Init random engine
+    srand(time(0));
+
+    // Create window
+    sf::RenderWindow window(sf::VideoMode(400, 400), "Minesweeper");
+
+    // Load tile sprite
+    sf::Texture texture;
+    texture.loadFromFile("assets/tiles.jpg");
+    sf::Sprite tiles(texture);
+
+    for(int i = 1; i <=10; ++i) {
+        for(int j = 1; j <=10; ++j) {
+            SHOWN[i][j] = 10;
+        }
+    }
+
+    while(window.isOpen()) {
+        // Process events
+        sf::Event e;
+        while (window.pollEvent((e))) {
+            // Handle closing
+            if (e.type == sf::Event::Closed) { window.close(); }
+        }
+        // Draw grid
+        window.clear(sf::Color::White);
+
+        for(int i = 1; i <=10; ++i) {
+            for(int j = 1; j <=10; ++j) {
+                tiles.setTextureRect(sf::IntRect(SHOWN[i][j] * WIDTH, 0, WIDTH, WIDTH));
+                tiles.setPosition(i * WIDTH, j * WIDTH);
+                window.draw(tiles);
+            }
+        }
+        window.display();
+    }
+
     return 0;
 }
