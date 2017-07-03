59461b5aa36f560cf02ea099eee6dc66d72ab4f8
vgomeniuk <vgomeniuk@lvivsoft.com>
2017-07-03 10:30:16 +0300

    Randomly set bombs and display correct hints for number of bombs around

--- main.cpp
+++ main.cpp
@@ -5,6 +5,33 @@ int WIDTH = 32;
 int GRID[12][12];
 int SHOWN[12][12];
 
+// Relative positions of surrounding cells
+int HINT_GRID[8][2] = {
+    {-1, -1},   {0, -1},    {1, -1},
+    {-1, 0},    /*0, 0*/    {1, 0},
+    {-1, 1},    {0, 1},     {1, 1}
+};
+
+
+void hints() {
+    for(int i = 1; i <=10; ++i) {
+        for(int j = 1; j <=10; ++j) {
+            // Skip bomb cells
+            if(GRID[i][j] == 9) { continue; }
+            int counter = 0;
+
+            // Count number of bombs in surrounding cells
+            for (auto position : HINT_GRID) {
+                if(GRID[i + position[0]][j + position[1]] == 9) {
+                    ++counter;
+                }
+            }
+            // Set proper hint number in GRID
+            GRID[i][j] = counter;
+        }
+    }
+}
+
 
 int main()
 {
@@ -22,8 +49,11 @@ int main()
     for(int i = 1; i <=10; ++i) {
         for(int j = 1; j <=10; ++j) {
             SHOWN[i][j] = 10;
+            // Randomly set as bomb or empty
+            (rand() % 5 == 0) ? GRID[i][j] = 9 : GRID[i][j] = 0;
         }
     }
+    hints();
 
     while(window.isOpen()) {
         // Process events
@@ -34,9 +64,9 @@ int main()
         }
         // Draw grid
         window.clear(sf::Color::White);
-
         for(int i = 1; i <=10; ++i) {
             for(int j = 1; j <=10; ++j) {
+                // Choose proper tile to draw and display it at position
                 tiles.setTextureRect(sf::IntRect(SHOWN[i][j] * WIDTH, 0, WIDTH, WIDTH));
                 tiles.setPosition(i * WIDTH, j * WIDTH);
                 window.draw(tiles);
